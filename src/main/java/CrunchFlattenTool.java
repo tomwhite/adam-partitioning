aaf9b1eec35566252cef173ab50ad23f0459e56d
Tom White <tom@cloudera.com>
2015-06-30 11:18:15 +0100

    Add a flatten tool.

--- src/main/java/CrunchFlattenTool.java
+++ src/main/java/CrunchFlattenTool.java
@@ -1,12 +1,13 @@
-import java.io.IOException;
-import java.io.InputStream;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
+import org.apache.crunch.DoFn;
+import org.apache.crunch.Emitter;
 import org.apache.crunch.PCollection;
 import org.apache.crunch.Pipeline;
 import org.apache.crunch.PipelineResult;
 import org.apache.crunch.Source;
 import org.apache.crunch.impl.mr.MRPipeline;
+import org.apache.crunch.types.avro.Avros;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -16,18 +17,15 @@ import org.kitesdk.data.CompressionType;
 import org.kitesdk.data.DatasetDescriptor;
 import org.kitesdk.data.Datasets;
 import org.kitesdk.data.Formats;
-import org.kitesdk.data.PartitionStrategy;
 import org.kitesdk.data.View;
 import org.kitesdk.data.crunch.CrunchDatasets;
-import org.kitesdk.data.spi.PartitionStrategyParser;
 
-public class CrunchPartitionTool extends Configured implements Tool {
+public class CrunchFlattenTool extends Configured implements Tool {
 
   @Override
   public int run(String[] args) throws Exception {
-    String partitionStrategyName = args[0];
-    String inputPath = args[1];
-    String outputPath = args[2];
+    String inputPath = args[0];
+    String outputPath = args[1];
 
     Configuration conf = getConf();
 
@@ -35,12 +33,16 @@ public class CrunchPartitionTool extends Configured implements Tool {
     Schema schema = SchemaUtils.readSchema(path, conf);
     Source source = SchemaUtils.readSource(path, conf, schema);
 
-    Pipeline pipeline = new MRPipeline(CrunchPartitionTool.class, conf);
+    Schema flatSchema = new Flattener().flatten(schema);
+
+    Pipeline pipeline = new MRPipeline(CrunchFlattenTool.class, conf);
     PCollection<GenericData.Record> records = pipeline.read(source);
 
+    PCollection<GenericData.Record> flatRecords = records.parallelDo(new
+        FlattenFn(flatSchema), Avros.generics(flatSchema));
+
     DatasetDescriptor desc = new DatasetDescriptor.Builder()
-        .schema(schema)
-        .partitionStrategy(readPartitionStrategy(partitionStrategyName))
+        .schema(flatSchema)
         .format(Formats.PARQUET)
         .compressionType(CompressionType.Uncompressed)
         .build();
@@ -48,30 +50,36 @@ public class CrunchPartitionTool extends Configured implements Tool {
     View<GenericData.Record> dataset = Datasets.create("dataset:" + outputPath, desc,
         GenericData.Record.class);
 
-    int numReducers = conf.getInt("mapreduce.job.reduces", 1);
-    System.out.println("Num reducers: " + numReducers);
-    PCollection<GenericData.Record> partition =
-        CrunchDatasets.partition(records, dataset, numReducers);
-
-    pipeline.write(partition, CrunchDatasets.asTarget(dataset));
+    pipeline.write(flatRecords, CrunchDatasets.asTarget(dataset));
 
     PipelineResult result = pipeline.done();
     return result.succeeded() ? 0 : 1;
   }
 
   public static void main(String[] args) throws Exception {
-    int exitCode = ToolRunner.run(new CrunchPartitionTool(), args);
+    int exitCode = ToolRunner.run(new CrunchFlattenTool(), args);
     System.exit(exitCode);
   }
 
-  private PartitionStrategy readPartitionStrategy(String name) throws IOException {
-    InputStream in = CrunchPartitionTool.class.getResourceAsStream(name + ".json");
-    try {
-      return PartitionStrategyParser.parse(in);
-    } finally {
-      if (in != null) {
-        in.close();
-      }
+  private static class FlattenFn extends DoFn<GenericData.Record, GenericData.Record> {
+
+    private String schemaString;
+    private transient Schema flatSchema;
+    private transient Flattener flattener;
+
+    public FlattenFn(Schema flatSchema) {
+      this.schemaString = flatSchema.toString();
+    }
+
+    @Override
+    public void initialize() {
+      flatSchema = Schema.parse(schemaString);
+      flattener = new Flattener();
+    }
+
+    @Override
+    public void process(GenericData.Record input, Emitter<GenericData.Record> emitter) {
+      emitter.emit(flattener.flattenRecord(flatSchema, input));
     }
   }
 }
